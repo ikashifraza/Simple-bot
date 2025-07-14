e3570f221ce8c7037e0ea2ea5f8e75e7de21d874
ikashifraza <141767314+ikashifraza@users.noreply.github.com>
2025-07-15 00:48:40 +0500

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,24 @@
+{
+  "name": "messenger-goibot",
+  "version": "1.0.0",
+  "description": "Facebook Messenger bot with goibot auto-reply using fca-unofficial",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "keywords": [
+    "messenger",
+    "bot",
+    "facebook",
+    "goibot",
+    "fca-unofficial"
+  ],
+  "author": "Kashif",
+  "license": "MIT",
+  "dependencies": {
+    "fca-unofficial": "^1.0.0",
+    "fs-extra": "^11.2.0",
+    "express": "^4.18.2",
+    "axios": "^1.6.8"
+  }
+}
